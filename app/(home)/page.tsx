b363070762967f3a4d0d9892b339b33e7fb31a9a
Marcus Silva <devsilvamarcus@gmail.com>
2026-05-13 15:16:58 -0300

    feat: add ExpensesPerCategory component and integrate it into the Home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,6 +4,7 @@ import { redirect } from 'next/navigation';
 
 import Navbar from '../_components/navbar';
 import { getDashboard } from '../_data/get-dashboard';
+import ExpensesPerCategory from './_components/expenses-per-category';
 import SummaryCards from './_components/summary-cards';
 import TimeSelect from './_components/time-select';
 import TransactionsPieChart from './_components/transactions-pie-chart';
@@ -44,6 +45,9 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
 
             <div className="grid grid-cols-3 grid-rows-1 gap-6">
               <TransactionsPieChart {...dashboard} />
+              <ExpensesPerCategory
+                expensesPerCategory={dashboard.totalExpensePerCategory}
+              />
             </div>
           </div>
         </div>
