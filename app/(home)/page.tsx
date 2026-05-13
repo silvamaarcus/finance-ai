cf1ba6c762cd5bf9fb38cd76c71b9ed91f4c3d8b
Marcus Silva <devsilvamarcus@gmail.com>
2026-05-13 15:35:44 -0300

    feat: add LastTransactions component and integrate it into the Home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,6 +5,7 @@ import { redirect } from 'next/navigation';
 import Navbar from '../_components/navbar';
 import { getDashboard } from '../_data/get-dashboard';
 import ExpensesPerCategory from './_components/expenses-per-category';
+import LastTransactions from './_components/last-transactions';
 import SummaryCards from './_components/summary-cards';
 import TimeSelect from './_components/time-select';
 import TransactionsPieChart from './_components/transactions-pie-chart';
@@ -33,23 +34,22 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
   return (
     <>
       <Navbar />
-      <div className="space-y-6 p-6">
+      <div className="flex h-full flex-col space-y-6 overflow-hidden p-6">
         <div className="flex justify-between">
           <h1 className="text-2xl font-bold">Dashboard</h1>
           <TimeSelect />
         </div>
-
-        <div className="grid-cols-[2fr, 1fr] grid">
-          <div className="flex flex-col gap-6">
+        <div className="grid h-full grid-cols-[2fr,1fr] gap-6 overflow-hidden">
+          <div className="flex flex-col gap-6 overflow-hidden">
             <SummaryCards month={month} {...dashboard} />
-
-            <div className="grid grid-cols-3 grid-rows-1 gap-6">
+            <div className="grid h-full grid-cols-3 grid-rows-1 gap-6 overflow-hidden">
               <TransactionsPieChart {...dashboard} />
               <ExpensesPerCategory
                 expensesPerCategory={dashboard.totalExpensePerCategory}
               />
             </div>
           </div>
+          <LastTransactions lastTransactions={dashboard.lastTransactions} />
         </div>
       </div>
     </>
